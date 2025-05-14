d928cda9478ca972e1c1bc522a1fe0afc7ecefd6
atoosh22 <olaad22@gmail.com>
2025-05-14 17:31:35 +0300

    setup

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class DashboardController : Controller
+    {
+        // GET: DashboardController
+        // public ActionResult Index()
+        // {
+        //     return View();
+        // }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Student()
+        {
+            return View();
+        }
+
+        public ActionResult Teacher()
+        {
+            return View();
+        }
+
+        public ActionResult Courses()
+        {
+            return View();
+        }
+
+        public ActionResult Clases()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
